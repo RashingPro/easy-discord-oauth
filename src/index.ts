1dc5e09226d6498bb3968c20b3b2956616691471
RashingPro <rashingpro@yandex.ru>
2025-05-02 10:54:25 +0300

    Additional fix for #1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import {DiscordOAuth} from "./interface.js";
-
 export default DiscordOAuth;
+
+export const exchangeCode = DiscordOAuth.exchangeCode;
+export const refreshToken = DiscordOAuth.refreshToken;
+export const revokeToken = DiscordOAuth.revokeToken;
+
+export const getCurrentUser = DiscordOAuth.User.getCurrentUser;
+export const getUser = DiscordOAuth.User.getUser;
+export const getCurrentUserGuilds = DiscordOAuth.User.getCurrentUserGuilds;
+export const getCurrentUserGuildMember = DiscordOAuth.User.getCurrentUserGuildMember;
